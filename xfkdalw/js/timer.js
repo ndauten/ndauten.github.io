c38cfdabac4bf74221e7f88422de2c62e5a8c2a6
Nathan Dautenhahn <ndd@rice.edu>
2023-03-26 14:16:44 -0500

    update deadlines with auto calc
    Signed-off-by: Nathan Dautenhahn <ndd@rice.edu>

--- xfkdalw/js/timer.js
+++ xfkdalw/js/timer.js
@@ -17,6 +17,7 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   const clock = document.getElementById(id);
+  clock.style.display = 'block';
   const daysSpan = clock.querySelector('.days');
   const hoursSpan = clock.querySelector('.hours');
   const minutesSpan = clock.querySelector('.minutes');
@@ -39,7 +40,48 @@ function initializeClock(id, endtime) {
   const timeinterval = setInterval(updateClock, 1000);
 }
 
-//const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-const deadline = 'March 17 2023 18:00:00 GMT-0600';
-initializeClock('clockdiv', deadline);
 
+//const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+//const deadline = 'March 17 2023 18:00:00 GMT-0600';
+const schedule = [    
+  //['Jul 25 2015', 'Sept 20 2015'],    
+  //['Sept 21 2015', 'Jul 25 2016'],    
+  //['Jul 25 2016', 'Jul 25 2030']
+  ['March 17 2023 8:00:00 GMT-0600','March 30 2023 18:00:00 GMT-0600']
+  ['April 6 2023 8:00:00 GMT-0600','April 10 2023 18:00:00 GMT-0600']
+  ['April 10 2023 8:00:00 GMT-0600','April 14 2023 18:00:00 GMT-0600']
+  ['April 17 2023 8:00:00 GMT-0600','April 20 2023 18:00:00 GMT-0600']
+  ['April 24 2023 8:00:00 GMT-0600','May 5 2023 18:00:00 GMT-0600']
+  ['May 8 2023 8:00:00 GMT-0600','May 19 2023 18:00:00 GMT-0600']
+  ['May 22 2023 8:00:00 GMT-0600','June 2 2023 18:00:00 GMT-0600']
+];
+
+//initializeClock('clockdiv', deadline);
+
+// iterate over each element in the schedule
+for (var i=0; i<schedule.length; i++) {
+  var startDate = schedule[i][0];
+  var endDate = schedule[i][1];
+
+  // put dates in milliseconds for easy comparisons
+  var startMs = Date.parse(startDate);
+  var endMs = Date.parse(endDate);
+  var currentMs = Date.parse(new Date());
+
+  // if current date is between start and end dates, display clock
+  if (endMs > currentMs && currentMs >= startMs ) {
+	initializeClock('clockdiv', endDate);
+  }
+}
+
+schedule.forEach(([startDate, endDate]) => {
+  // put dates in milliseconds for easy comparisons
+  const startMs = Date.parse(startDate);
+  const endMs = Date.parse(endDate);
+  const currentMs = Date.parse(new Date());
+
+  // if current date is between start and end dates, display clock
+  if (endMs > currentMs && currentMs >= startMs ) {
+	initializeClock('clockdiv', endDate);
+  }
+}
